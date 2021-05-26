66cab2fbbeb947b69377e7dc10131fdda4d0bd00
Abdul Hafiz Muttaqien <abdhafizmuttaqien@gmail.com>
2021-05-26 10:47:53 +0700

    clean code edit blade

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -37,8 +37,8 @@
                 <img class="logo" src="{{ asset('img/logo.png')}}">
                 Project Qtasnim
             </a>
-            <a href="{{url('/transaksi')}}" class="item">Transaksi</a>
-            <a href="{{url('/barang')}}" class="item">Barang</a>
+            <!-- <a href="{{url('/transaksi')}}" class="item">Transaksi</a>
+            <a href="{{url('/barang')}}" class="item">Barang</a> -->
         </div>
     </div>
     
@@ -46,16 +46,9 @@
         <div class="ui center aligned header">
             <h1>Ubah Data</h1>
         </div>
-        <!-- <div class="ui container"  style="margin-bottom:1.5em;">
-            <a id="input_modal" href="#" class="ui primary labeled icon button">
-                <i class="boxes icon"></i>
-                Tambah Data
-            </a>                     
-        </div> -->
-        
+
         <form method="post" action="/ubah/{{$data->id}}" class="ui form">
             @csrf
-                
                 <div class="field">
                     <label>Nama Barang</label>
                     <input type="text" id="nama_barang" name="nama_barang" value="{{$data->nama_barang}}">
@@ -77,10 +70,6 @@
         
     </div>   
 
-    
-
-
-
     <div class="ui inverted vertical footer segment">
         <div class="ui center aligned container">
             <div class="ui inverted section"></div>
@@ -94,26 +83,11 @@
         </div>
     </div>
     
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
-    -->
     <script src="{{ asset('semantic/jquery.min.js') }}"></script>
-    <!-- <script scr="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script> -->
     <script src="{{ asset('DataTables/DataTables-1.10.24/js/jquery.dataTables.min.js') }}"></script>
-    <!-- <script scr="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script> -->
     <script src="{{ asset('DataTables/DataTables-1.10.24/js/dataTables.semanticui.min.js') }}"></script>
-    <!-- <script scr="https://cdn.datatables.net/1.10.24/js/dataTables.semanticui.min.js"></script> -->
     <script src="{{ asset('semantic/semantic.min.js') }}"></script>
-    <!-- <script scr="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.js"></script> -->
-
-    
+  
     <script>
         $(document).ready(function() {
         $('#example').DataTable();
@@ -124,66 +98,6 @@
         $( "#input_modal" ).click(function() {
             $("#modalinput").modal('show');
         });
-    </script>
-
-<script>
-        $( "#edit_modal" ).click(function() {
-            // var user_id = $(this).data('id');
-            $.ajax({
-                type: 'GET',
-                url: "{{url('/edit/5')}}",
-                dataType: 'json',
-                contentType: 'application/json',
-                // data: {
-                //     date: date
-                // },
-                success: function(data){
-                    console.log(data.data);
-                    $("#modaledit").modal('show');
-                    $('#id').val(data.data.id);
-                    $('#nama_barang').val(data.data.nama_barang);
-                    $('#stok').val(data.data.stok);
-                    $('#jumlah_terjual').val(data.data.jumlah_terjual);
-                    $('#jenis_barang').val(data.data.jenis_barang);
-                    $('#id').val(data.data.id);
-                    var id  = data.data.id;
-                    console.log(id);
-                },
-                error: function(xhr){
-                    console.log(xhr.responseText);
-                }
-            });
-        });
-
-        $( "#update_button" ).click(function() {
-            // var user_id = $(this).data('id');
-            var id = document.getElementById("#id").value;
-            console.log(id);
-            // $.ajax({
-            //     type: 'GET',
-            //     url: "{{url('/edit/5')}}",
-            //     dataType: 'json',
-            //     contentType: 'application/json',
-            //     // data: {
-            //     //     date: date
-            //     // },
-            //     success: function(data){
-            //         console.log(data.data);
-            //         $("#modaledit").modal('show');
-            //         $('#id').val(data.data.id);
-            //         $('#nama_barang').val(data.data.nama_barang);
-            //         $('#stok').val(data.data.stok);
-            //         $('#jumlah_terjual').val(data.data.jumlah_terjual);
-            //         $('#jenis_barang').val(data.data.jenis_barang);
-            //         var id  = data.data.id;
-            //         console.log(id);
-            //     },
-            //     error: function(xhr){
-            //         console.log(xhr.responseText);
-            //     }
-            // });
-        });
-
     </script>
 
   </body>
